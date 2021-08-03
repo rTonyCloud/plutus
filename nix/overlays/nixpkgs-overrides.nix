0c85399845f2ad46c55880f9b872b73efceba3ef
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-08-03 20:09:53 +1200

    Remove morph override (nixpkgs pin now includes the fix)

--- nix/overlays/nixpkgs-overrides.nix
+++ nix/overlays/nixpkgs-overrides.nix
@@ -1,17 +1,4 @@
 self: super: {
-  morph = super.morph.overrideAttrs (old: {
-    # See https://github.com/DBCDK/morph/pull/141
-    # Note that this patch does refelct the original content
-    # of the PR above since it was broken.
-    patches = [ ../pkgs/morph/sshopts.patch ];
-    src = self.fetchFromGitHub {
-      owner = "DBCDK";
-      repo = "morph";
-      rev = "c048d6339f18613a1544bc62ff852cb4c6de1042";
-      sha256 = "0yb8prji2nqjsj1aiiqnbqaajbi5l17rg8k78ry7pl3a8sqa3h1x";
-    };
-  });
-
   python3 = super.python3.override {
     packageOverrides = python-self: python-super: {
       # New version has much better citation styles
